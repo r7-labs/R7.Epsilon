f9b6b34081f7b8c009d870fc6163f8b5914189e6
Roman M. Yagodin <roman.yagodin@gmail.com>
2014-11-19 15:18:32 +0400

    Begin work on Simple skin with no modal frames to meet some W3C accesibility recomendations

---
+++ R7.Epsilon/Skins/js/simple.js
@@ -0,0 +1,11 @@
+﻿$(function() {
+    // replace all popup links with simple ones
+    $("a[href^='javascript:dnnModal']").each (function () {
+        var url = $(this).attr ("href");
+        var urlEnd = url.indexOf ("?popUp=");
+        var urlStart = url.indexOf ("http");
+        if (urlStart >= 0 && urlEnd >= 0) {
+            $(this).attr ("href", url.substring (urlStart, urlEnd));
+        }
+    });
+});
