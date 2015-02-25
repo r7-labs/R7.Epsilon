b832baa7df433d7c05aa5e72df3c3945d3b80e78
roman-yagodin <roman.yagodin@gmail.com>
2015-02-25 16:03:02 +0400

    Use more specific div.sub selector

--- R7.Epsilon/Menu/Mega2Epsilon.js
+++ R7.Epsilon/Menu/Mega2Epsilon.js
@@ -1,5 +1,5 @@
 function splitSubMenu(columns) {
-	jQuery("div.sub").each(function (i) {
+	jQuery("ul.megamenu div.sub").each(function (i) {
 		var items = jQuery(this).find('ul');
 		var blockCount = columns;
 		for (var i = 0; i < items.length; i += blockCount) {
