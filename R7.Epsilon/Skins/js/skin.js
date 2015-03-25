178591d6e6779126633c7733d3f42abe113d963b
roman-yagodin <roman.yagodin@gmail.com>
2015-03-25 12:11:48 +0400

    Hide search until jQuery rework

--- R7.Epsilon/Skins/js/skin.js
+++ R7.Epsilon/Skins/js/skin.js
@@ -5,9 +5,9 @@
 $(function() {
     // "Bootstrapify" search
     $(".skin-search .searchInputContainer").attr ("style", "display:table-cell !important");
-    $(".skin-search #dnn_dnnSearch_ClassicSearch").addClass ("input-group");
     $(".skin-search #dnn_dnnSearch_txtSearch").removeClass ("NormalTextBox").addClass("form-control");
     $(".skin-search #dnn_dnnSearch_cmdSearch").addClass ("btn btn-default").wrap ("<span class='input-group-btn'></span>");
+    $(".skin-search > span:first-child").addClass ("input-group").show ();
 });
 
 $(function() {
