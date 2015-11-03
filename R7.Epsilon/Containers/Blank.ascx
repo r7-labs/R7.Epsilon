1cb04c4b74ab4e41b6a1a81093972d0508941fcb
roman-yagodin <roman.yagodin@gmail.com>
2015-11-03 12:18:45 +0300

    Add editable module titles for Blank and Thumbnail  containers

--- R7.Epsilon/Containers/Blank.ascx
+++ R7.Epsilon/Containers/Blank.ascx
@@ -1,4 +1,8 @@
 ﻿<%@ Control Language="C#" AutoEventWireup="false" Inherits="R7.Epsilon.EpsilonContainerBase" %>
+<%@ Register TagPrefix="dnn" TagName="TITLE" Src="~/Admin/Containers/Title.ascx" %>
 <div class="cnt-blank dnnClear">
+    <% if (IsEditable) { %>
+    <h4><dnn:TITLE ID="dnnTITLE" runat="server" CssClass="h4" /></h4>
+    <% } %>
     <div id="ContentPane" runat="server"></div>
 </div>
--- R7.Epsilon/components/EpsilonContainerBase.cs
+++ R7.Epsilon/components/EpsilonContainerBase.cs
@@ -26,14 +26,63 @@
 
 using System;
 using DotNetNuke.UI.Containers;
+using DotNetNuke.Security.Permissions;
+using DotNetNuke.UI.Skins.Controls;
+using DotNetNuke.Common;
+using DotNetNuke.Security;
+using DotNetNuke.Entities.Modules;
 
 namespace R7.Epsilon
 {
     public class EpsilonContainerBase: Container
     {
-		protected override void OnLoad (EventArgs e)
-		{
-			base.OnLoad (e);
-		}
+        protected override void OnLoad (EventArgs e)
+        {
+            base.OnLoad (e);
+        }
+
+        private bool? _isEditable;
+
+        // Source: https://github.com/dnnsoftware/Dnn.Platform/blob/development/DNN%20Platform/Library/UI/Modules/ModuleInstanceContext.cs
+        public bool IsEditable
+        {
+            get
+            {
+                ModuleInfo _configuration = null;
+
+                // Perform tri-state switch check to avoid having to perform a security
+                // role lookup on every property access (instead caching the result)
+                if (_isEditable == null)
+                {
+                    var blnPreview = (PortalSettings.UserMode == DotNetNuke.Entities.Portals.PortalSettings.Mode.View);
+                    if (Globals.IsHostTab (PortalSettings.ActiveTab.TabID))
+                    {
+                        blnPreview = false;
+                    }
+
+                    // get current module info
+                    if (_configuration == null)
+                    {
+                        var moduleIdStr = ID.Replace ("ctr", string.Empty);
+                        int moduleId;
+                        if (int.TryParse (moduleIdStr, out moduleId))
+                        {
+                            _configuration = ModuleController.Instance.GetModule (moduleId, PortalSettings.ActiveTab.TabID, false);
+                        }
+                    }
+
+                    var blnHasModuleEditPermissions = false;
+                    if (_configuration != null)
+                    {
+                        blnHasModuleEditPermissions = ModulePermissionController.HasModuleAccess (
+                            SecurityAccessLevel.Edit, "CONTENT", _configuration);
+                    }
+
+                    _isEditable = !blnPreview && blnHasModuleEditPermissions;
+                }
+
+                return _isEditable.Value;
+            }
+        }
     }
 }
