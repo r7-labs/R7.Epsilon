a953add5761f4311cf06dba3291950b18a9dca85
roman-yagodin <roman.yagodin@gmail.com>
2015-11-03 13:40:46 +0300

    Add dependency to DNN 7.4.0, set version to 1.5.0, update release notes

--- R7.Epsilon/Properties/AssemblyInfo.cs
+++ R7.Epsilon/Properties/AssemblyInfo.cs
@@ -23,6 +23,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
+
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
@@ -42,7 +43,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion ("1.0.4.*")]
+[assembly: AssemblyVersion ("1.5.0.*")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
