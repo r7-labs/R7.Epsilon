29ff0f759b149e79aff34c23f1ccadad27d2c3e1
roman-yagodin <roman.yagodin@gmail.com>
2016-04-21 13:51:26 +0300

    Remove HammerFlex-related XML comments

--- R7.Epsilon/Skins/Universal.ascx.cs
+++ R7.Epsilon/Skins/Universal.ascx.cs
@@ -33,19 +33,6 @@ using DotNetNuke.Services.Localization;
 
 namespace R7.Epsilon
 {
-    /// -----------------------------------------------------------------------------
-    /// <summary>
-    /// The View class displays the content
-    /// 
-    /// Typically your view control would be used to display content or functionality in your module.
-    /// 
-    /// View may be the only control you have in your project depending on the complexity of your module
-    /// 
-    /// Because the control inherits from HammerFlexModuleBase you have access to any custom properties
-    /// defined there, as well as properties from DNN such as PortalId, ModuleId, TabId, UserId and many more.
-    /// 
-    /// </summary>
-    /// -----------------------------------------------------------------------------
     public partial class Universal : EpsilonSkinBase
     {
         public Universal ()
