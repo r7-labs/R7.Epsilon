072dbbfbc22466788ed46e6bbf1ce7299ebb55eb
roman-yagodin <roman.yagodin@gmail.com>
2016-07-21 13:52:35 +0300

    Fix #34: Register and Login forms should not open in popup in the a11y mode

--- R7.Epsilon/Skins/js/a11y.js
+++ R7.Epsilon/Skins/js/a11y.js
@@ -3,9 +3,14 @@
     $("a[href^='javascript:dnnModal']").each (function () {
         var url = $(this).attr ("href");
         var urlEnd = url.indexOf ("?popUp=");
-        var urlStart = url.indexOf ("http");
+        var urlStart = url.indexOf ("http://");
         if (urlStart >= 0 && urlEnd >= 0) {
             $(this).attr ("href", url.substring (urlStart, urlEnd));
         }
     });
+
+    // remove onclick handlers from login/register links
+    $("a[id$='_loginLink']").prop ("onclick", null).off ("click");
+    $("a[id$='_enhancedLoginLink']").prop ("onclick", null).off ("click");
+    $("a[id$='_enhancedRegisterLink']").prop ("onclick", null).off ("click");
 });
