c6cceb911c03013575a237768b0a236dbb93d6a1
roman-yagodin <roman.yagodin@gmail.com>
2016-08-31 14:04:48 +0300

    Add SelectLayout control template

---
+++ R7.Epsilon.LayoutManager/SelectLayout.ascx
@@ -0,0 +1,20 @@
+﻿<%@ Control Language="C#" AutoEventWireup="false" CodeBehind="EditLayout.ascx.cs" Inherits="R7.Epsilon.LayoutManager.SelectLayout" %>
+<%@ Register TagPrefix="dnn" TagName="Label" Src="~/controls/labelcontrol.ascx" %>
+<%@ Register TagPrefix="dnn" TagName="TextEditor" Src="~/controls/TextEditor.ascx"%>
+<%@ Register TagPrefix="dnn" TagName="Audit" Src="~/controls/ModuleAuditControl.ascx" %>
+<%@ Register TagPrefix="dnn" Namespace="DotNetNuke.Web.UI.WebControls" Assembly="DotNetNuke.Web" %>
+<%@ Register TagPrefix="dnn" Namespace="DotNetNuke.Web.Client.ClientResourceManagement" Assembly="DotNetNuke.Web.Client" %>
+
+<dnn:DnnCssInclude runat="server" FilePath="~/DesktopModules/R7.Epsilon/R7.Epsilon.LayoutManager/admin.css" Priority="200" />
+<div class="dnnForm dnnClear">
+    <fieldset>
+		<div class="dnnFormItem">
+            <dnn:Label id="labelLayout" runat="server" ControlName="comboLayout" />  
+            <asp:DropDownList id="comboLayout" runat="server" />
+        </div>
+    	<ul class="dnnActions dnnClear">
+            <li><asp:LinkButton id="buttonSelect" runat="server" CssClass="dnnPrimaryAction" resourcekey="Select.Text" OnClick="buttonSelect_Click" /></li>
+            <li><asp:HyperLink id="linkCancel" runat="server" CssClass="dnnSecondaryAction" resourcekey="cmdCancel" /></li>
+        </ul>
+    </fieldset>
+</div>
---
+++ R7.Epsilon.LayoutManager/SelectLayout.ascx.cs
@@ -0,0 +1,104 @@
+﻿//
+// EditR7.Epsilon.LayoutManager.ascx.cs
+//
+// Author:
+//       Roman M. Yagodin <roman.yagodin@gmail.com>
+//
+// Copyright (c) 2016 Roman M. Yagodin
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.IO;
+using DotNetNuke.Common;
+using DotNetNuke.Entities.Modules;
+using DotNetNuke.Services.Exceptions;
+using DotNetNuke.Services.Localization;
+using DotNetNuke.UI.Skins;
+using DotNetNuke.UI.Skins.Controls;
+using R7.Epsilon.LayoutManager.Components;
+
+namespace R7.Epsilon.LayoutManager
+{
+    public partial class SelectLayout : PortalModuleBase
+    {
+        #region Handlers
+
+        /// <summary>
+        /// Handles Init event for a control.
+        /// </summary>
+        /// <param name="e">Event args.</param>
+        protected override void OnInit (EventArgs e)
+        {
+            base.OnInit (e);
+
+            linkCancel.NavigateUrl = GetReturnUrl ();
+        }
+
+        /// <summary>
+        /// Handles Load event for a control.
+        /// </summary>
+        /// <param name="e">Event args.</param>
+        protected override void OnLoad (EventArgs e)
+        {
+            base.OnLoad (e);
+
+            try {
+                if (!IsPostBack) {
+                }
+            } 
+            catch (Exception ex) {
+                Exceptions.ProcessModuleLoadException (this, ex);
+            }
+        }
+
+        /// <summary>
+        /// Handles Click event for Select button
+        /// </summary>
+        /// <param name='sender'>
+        /// Sender.
+        /// </param>
+        /// <param name='e'>
+        /// Event args.
+        /// </param>
+        protected void buttonSelect_Click (object sender, EventArgs e)
+        {
+            try {
+                ModuleController.SynchronizeModule (ModuleId);
+                Response.Redirect (GetReturnUrl (), true);
+            } 
+            catch (Exception ex) {
+                Exceptions.ProcessModuleLoadException (this, ex);
+            }
+        }
+
+        #endregion
+
+        protected string GetReturnUrl ()
+        {
+            var returnTabIdStr = Request.QueryString ["returntabid"];
+            if (!string.IsNullOrEmpty (returnTabIdStr)) {
+                return Globals.NavigateURL (int.Parse (returnTabIdStr));
+            }
+
+            return Globals.NavigateURL ();
+        }
+    }
+}
+
