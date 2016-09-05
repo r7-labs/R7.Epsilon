1ae70dbbd2a3411eed3677bad795b0b61ae47d82
roman-yagodin <roman.yagodin@gmail.com>
2016-09-05 14:40:38 +0300

    Add some tests for LayoutValidator

---
+++ R7.Epsilon.Tests/Components/LayoutValidatorTests.cs
@@ -0,0 +1,64 @@
+﻿//
+//  LayoutValidatorTests.cs
+//
+//  Author:
+//       Roman M. Yagodin <roman.yagodin@gmail.com>
+//
+//  Copyright (c) 2016 Roman M. Yagodin
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using Xunit;
+using R7.Epsilon.Components;
+
+namespace R7.Epsilon.Tests.Components
+{
+    public class LayoutValidatorTests
+    {
+        [Fact]
+        public void LayoutValidatorFormattingTest ()
+        {
+            // formatting error - no closing tag
+            Assert.False (LayoutValidator.IsValid (
+                @"<div class=""row"">
+                    <div id=""ContentPane"" runat=""server"" /> 
+                <div>"
+            ));
+        }
+
+        [Fact]
+        public void LayoutValidatorContentPaneTest ()
+        {
+            // no content pane
+            Assert.False (LayoutValidator.IsValid (
+                @"<div class=""row"">
+                    <div id=""AsidePane"" runat=""server"" /> 
+                </div>"
+            ));
+        }
+
+        [Fact]
+        public void LayoutValidatorUniquePanesTest ()
+        {
+            // two panes with same 
+            Assert.False (LayoutValidator.IsValid (
+                @"<div class=""row"">
+                    <div id=""ContentPane"" runat=""server"" />
+                    <div id=""AsidePane"" runat=""server"" />
+                    <div id=""AsidePane"" runat=""server"" />
+                </div>"
+            ));
+        }
+    }
+}
