30d9df1e3fdfdc4fc000c55211c854bab570976a
roman-yagodin <roman.yagodin@gmail.com>
2016-09-05 13:40:38 +0300

    Close #32: Add skin for 404 and 500 pages stripped from login/user controls and many other elements

---
+++ R7.Epsilon/Skins/ErrorSkin.ascx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using DotNetNuke.Security;
+using DotNetNuke.Services.Exceptions;
+using DotNetNuke.Entities.Modules;
+using DotNetNuke.Entities.Modules.Actions;
+using DotNetNuke.Services.Localization;
+using R7.Epsilon.Components;
+
+namespace R7.Epsilon
+{
+    public partial class ErrorSkin : EpsilonSkinBase
+    {
+        public ErrorSkin ()
+		{
+			
+		}    
+    }
+}
