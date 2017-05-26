a107d44a6742df5fcaff7d04e04cf72e6fbceb04
roman-yagodin <roman.yagodin@gmail.com>
2017-05-26 11:37:42 +0300

    Increase hoverIntent onMouseOut timeout to 500 ms

--- R7.Epsilon/Skins/js/menu.js
+++ R7.Epsilon/Skins/js/menu.js
@@ -136,7 +136,7 @@ function skinSplitSubMenu($, controlId, columns) {
             sensitivity: 2, // number = sensitivity threshold (must be 1 or higher)    
             interval: 100, // number = milliseconds for onMouseOver polling interval    
             over: megaHoverOver, // function = onMouseOver callback (REQUIRED)    
-            timeout: 100, // number = milliseconds delay before onMouseOut    
+            timeout: 500, // number = milliseconds delay before onMouseOut    
             out: megaHoverOut // function = onMouseOut callback (REQUIRED)    
         });
 
