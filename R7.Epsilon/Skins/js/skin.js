5feeb0e1a8046926488748b38475b7ad8584af67
roman-yagodin <roman.yagodin@gmail.com>
2019-06-26 15:32:47 +0400

    GH-111 Display num of messages/notifications as bagdes

--- R7.Epsilon/Skins/js/skin.js
+++ R7.Epsilon/Skins/js/skin.js
@@ -4,6 +4,27 @@
 //
 //  Author: Roman M. Yagodin <roman.yagodin@gmail.com>
 //
+//  Copyright (c) 2014-2019 Roman M. Yagodin, R7.Labs
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+//
+//  File: skin.js
+//  Project: R7.Epsilon
+//
+//  Author: Roman M. Yagodin <roman.yagodin@gmail.com>
+//
 //  Copyright (c) 2015-2019 Roman M. Yagodin, R7.Labs
 //
 //  This program is free software: you can redistribute it and/or modify
@@ -136,6 +157,9 @@ window.skinOpenFeedback = function (button, $, feedbackModuleId) {
                 .prepend (profileImgBlock)
                 .append (divider)
                 .append (loginGroup.children ("a").prepend ("<i class='fas fa-lock'></i> "));
+
+                $(".skin-login li.userMessages > a > span").addClass ("badge badge-primary");
+                $(".skin-login li.userNotifications > a > span").addClass ("badge badge-secondary");
         }
         else {
             $(".skin-login ul.buttonGroup")
