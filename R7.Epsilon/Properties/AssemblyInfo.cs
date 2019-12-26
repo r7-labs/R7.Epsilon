2d164193bac145afc5d1d3d2977464fa20da9584
roman-yagodin <roman.yagodin@gmail.com>
2019-12-26 14:47:29 +0400

    Bump version

--- R7.Epsilon/Properties/AssemblyInfo.cs
+++ R7.Epsilon/Properties/AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Runtime.CompilerServices;
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
 [assembly: AssemblyVersion ("2.0.0.*")]
-[assembly: AssemblyInformationalVersion ("2.0-alpha.1")]
+[assembly: AssemblyInformationalVersion ("2.0-beta.1")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
