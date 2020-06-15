4df831aa6af0e680aff2732039d6cf5088ce5379
roman-yagodin <roman.yagodin@gmail.com>
2020-06-15 20:19:22 +0400

    Bump version

--- R7.Epsilon/Properties/AssemblyInfo.cs
+++ R7.Epsilon/Properties/AssemblyInfo.cs
@@ -16,8 +16,8 @@
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion ("2.0.5")]
-[assembly: AssemblyInformationalVersion ("2.0.5-ci.1")]
+[assembly: AssemblyVersion ("2.0.6")]
+[assembly: AssemblyInformationalVersion ("2.0.6-ci.2")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
