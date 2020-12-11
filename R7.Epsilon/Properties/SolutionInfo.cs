5e4bb420b9b93d7e3b689d8acf86b39c3015e78e
roman-yagodin <roman.yagodin@gmail.com>
2020-12-11 11:27:15 +0400

    Extract SolutionInfo.cs

---
+++ R7.Epsilon/Properties/SolutionInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany ("R7.Labs")]
+[assembly: AssemblyProduct ("R7.Epsilon")]
+[assembly: AssemblyCopyright ("Roman M. Yagodin")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyVersion ("2.2.0")]
+[assembly: AssemblyInformationalVersion ("2.2.0")]
