75e3c79e9784f612869fc50e4ac0c8fcb9249437
roman-yagodin <roman.yagodin@gmail.com>
2021-01-14 10:27:15 +0300

    Bump version

--- R7.Epsilon/Properties/SolutionInfo.cs
+++ R7.Epsilon/Properties/SolutionInfo.cs
@@ -4,5 +4,5 @@
 [assembly: AssemblyProduct ("R7.Epsilon")]
 [assembly: AssemblyCopyright ("Roman M. Yagodin")]
 [assembly: AssemblyTrademark ("")]
-[assembly: AssemblyVersion ("2.3.0")]
-[assembly: AssemblyInformationalVersion ("2.3.0")]
+[assembly: AssemblyVersion ("2.3.1")]
+[assembly: AssemblyInformationalVersion ("2.3.1")]
